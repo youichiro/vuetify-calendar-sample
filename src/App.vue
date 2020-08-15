aab6abb1226fdcb73419b77e5150388e78aea68a
youichiro <cinnamon416@gmail.com>
2020-08-15 19:43:26 +0900

    [exec] npm run prettier

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <Calendar/>
+      <Calendar />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Calendar from './components/Calendar'
+import Calendar from './components/Calendar';
 
 export default {
   name: 'App',
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <v-sheet
-      tile
-      height="6vh"
-      color="grey lighten-3"
-      class="d-flex align-center"
-    >
+    <v-sheet tile height="6vh" color="grey lighten-3" class="d-flex align-center">
       <v-btn outlined class="ma-4" @click="setToday">
         今日
       </v-btn>
@@ -30,79 +25,79 @@
 </template>
 
 <script>
-  import moment from 'moment'
+import moment from 'moment';
 
-  export default {
-    data: () => ({
-      events: [],
-      value: moment().format('yyyy-MM-DD'),
-    }),
-    computed: {
-      title () {
-        return moment(this.value).format('yyyy年 MM月')
-      },
+export default {
+  data: () => ({
+    events: [],
+    value: moment().format('yyyy-MM-DD'),
+  }),
+  computed: {
+    title() {
+      return moment(this.value).format('yyyy年 MM月');
     },
-    methods: {
-      setToday () {
-        this.value = moment().format('yyyy-MM-DD')
-      },
-      getEvents () {
-        const events = [
-          {
-            name: '会議',
-            start: new Date('2020-08-03T10:00:00'),  // 開始時刻
-            end: new Date('2020-08-03T11:00:00'),  // 終了時刻
-            color: 'blue',
-            timed: true,  // 終日ならfalse
-          },
-          {
-            name: '休暇',
-            start: new Date('2020-08-04'),
-            end: new Date('2020-08-04'),
-            color: 'green',
-            timed: false,
-          },
-          {
-            name: '出張',
-            start: new Date('2020-08-05'),
-            end: new Date('2020-08-07'),
-            color: 'cyan',
-            timed: false,
-          },
-          {
-            name: '飲み会',
-            start: new Date('2020-08-06'),
-            end: new Date('2020-08-06'),
-            color: 'orange',
-            timed: false,
-          },
-          {
-            name: '打ち合わせ',
-            start: new Date('2020-08-07T10:00:00'),
-            end: new Date('2020-08-07T11:00:00'),
-            color: 'cyan',
-            timed: false,
-          },
-          {
-            name: '振り返り',
-            start: new Date('2020-08-07:11:00:00'),
-            end: new Date('2020-08-07T12:00:00'),
-            color: 'cyan',
-            timed: false,
-          },
-          {
-            name: '休暇',
-            start: new Date('2020-09-07'),
-            end: new Date('2020-09-11'),
-            color: 'green',
-            timed: false,
-          },
-        ]
-        this.events = events
-      },
-      getEventColor (event) {
-        return event.color
-      },
+  },
+  methods: {
+    setToday() {
+      this.value = moment().format('yyyy-MM-DD');
     },
-  }
+    getEvents() {
+      const events = [
+        {
+          name: '会議',
+          start: new Date('2020-08-03T10:00:00'), // 開始時刻
+          end: new Date('2020-08-03T11:00:00'), // 終了時刻
+          color: 'blue',
+          timed: true, // 終日ならfalse
+        },
+        {
+          name: '休暇',
+          start: new Date('2020-08-04'),
+          end: new Date('2020-08-04'),
+          color: 'green',
+          timed: false,
+        },
+        {
+          name: '出張',
+          start: new Date('2020-08-05'),
+          end: new Date('2020-08-07'),
+          color: 'cyan',
+          timed: false,
+        },
+        {
+          name: '飲み会',
+          start: new Date('2020-08-06'),
+          end: new Date('2020-08-06'),
+          color: 'orange',
+          timed: false,
+        },
+        {
+          name: '打ち合わせ',
+          start: new Date('2020-08-07T10:00:00'),
+          end: new Date('2020-08-07T11:00:00'),
+          color: 'cyan',
+          timed: false,
+        },
+        {
+          name: '振り返り',
+          start: new Date('2020-08-07:11:00:00'),
+          end: new Date('2020-08-07T12:00:00'),
+          color: 'cyan',
+          timed: false,
+        },
+        {
+          name: '休暇',
+          start: new Date('2020-09-07'),
+          end: new Date('2020-09-11'),
+          color: 'green',
+          timed: false,
+        },
+      ];
+      this.events = events;
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+  },
+};
 </script>
